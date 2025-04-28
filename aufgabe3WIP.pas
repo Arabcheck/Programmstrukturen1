135655ac94a7123417fc3de35cba9908c4786107
Luka Pablo Pervan <luka@MacBook-Air-von-Luka.local>
2025-04-28 15:07:38 +0200

    AI test

--- aufgabe3WIP.pas
+++ aufgabe3WIP.pas
@@ -1,252 +1,167 @@
-program aufgabe3WIP;
+program aufgabe3Perfekt;
 {$R+,Q+,X-}
 uses
-  SysUtils, Math;
+  SysUtils;
 
 var
-  Startbahnhof: Byte;
-  Zielbahnhof: Byte;
-  Zwischenstation: Byte;
-  Zwischenstationen: Byte;
+  Startbahnhof, Zielbahnhof, Zwischenstation: Byte;
   Kosten: Integer;
-  KostenZwischenstation: Integer;
-  KostenInsgesamt: Integer;
-  Benachbart: Boolean;
-  Schnellverbindung: Boolean;
-  Temporaer: Byte;
-  BenachbartZwischenstation: Boolean;
-  SchnellverbindungZwischenstation: Boolean;
-  TemporaerZwischenstation: Byte;
+  DirektGefunden: Boolean;
 
 const
   ZWISCHENSTATIONPREIS = 70;
   SCHNELLVERBINDUNGPREIS = 100;
 
 begin
-  Startbahnhof := 0;
-  Zielbahnhof := 0;
-  Zwischenstation := 0;
-  Zwischenstationen := 0;
-  Kosten := 0;
-  KostenZwischenstation := 0;
-  KostenInsgesamt := 0;
-  
-  Benachbart := False;
-  Schnellverbindung := False;
-  BenachbartZwischenstation := False;
-  SchnellverbindungZwischenstation := False;
-
-  Writeln('Geben sie an von welchem Bahnhof sie starten');
+  Writeln('Geben Sie den Startbahnhof ein (1-17):');
   Readln(Startbahnhof);
-  Writeln('Geben sie an zu welchen Bahnhof sie reisen möchten');
+  Writeln('Geben Sie den Zielbahnhof ein (1-17):');
   Readln(Zielbahnhof);
 
-  if Startbahnhof > Zielbahnhof then
+  if Startbahnhof = Zielbahnhof then
   begin
-    Temporaer := Startbahnhof;
-    Startbahnhof := Zielbahnhof;
-    Zielbahnhof := Temporaer;
+    Writeln('Start- und Zielbahnhof dürfen nicht gleich sein.');
+    Halt;
   end;
 
-  if Zwischenstation > Zielbahnhof then
+  Kosten := 0;
+  DirektGefunden := False;
+
+  { Direkte Verbindung prüfen (Gelb = 70 €, Grün = 100 €) }
+  if ((Startbahnhof = 1) and (Zielbahnhof = 3)) or ((Startbahnhof = 3) and (Zielbahnhof = 1)) or
+     ((Startbahnhof = 2) and (Zielbahnhof = 3)) or ((Startbahnhof = 3) and (Zielbahnhof = 2)) or
+     ((Startbahnhof = 2) and (Zielbahnhof = 5)) or ((Startbahnhof = 5) and (Zielbahnhof = 2)) or
+     ((Startbahnhof = 3) and (Zielbahnhof = 4)) or ((Startbahnhof = 4) and (Zielbahnhof = 3)) or
+     ((Startbahnhof = 3) and (Zielbahnhof = 6)) or ((Startbahnhof = 6) and (Zielbahnhof = 3)) or
+     ((Startbahnhof = 4) and (Zielbahnhof = 6)) or ((Startbahnhof = 6) and (Zielbahnhof = 4)) or
+     ((Startbahnhof = 5) and (Zielbahnhof = 7)) or ((Startbahnhof = 7) and (Zielbahnhof = 5)) or
+     ((Startbahnhof = 6) and (Zielbahnhof = 7)) or ((Startbahnhof = 7) and (Zielbahnhof = 6)) or
+     ((Startbahnhof = 7) and (Zielbahnhof = 10)) or ((Startbahnhof = 10) and (Zielbahnhof = 7)) or
+     ((Startbahnhof = 8) and (Zielbahnhof = 9)) or ((Startbahnhof = 9) and (Zielbahnhof = 8)) or
+     ((Startbahnhof = 8) and (Zielbahnhof = 12)) or ((Startbahnhof = 12) and (Zielbahnhof = 8)) or
+     ((Startbahnhof = 9) and (Zielbahnhof = 15)) or ((Startbahnhof = 15) and (Zielbahnhof = 9)) or
+     ((Startbahnhof = 10) and (Zielbahnhof = 11)) or ((Startbahnhof = 11) and (Zielbahnhof = 10)) or
+     ((Startbahnhof = 12) and (Zielbahnhof = 13)) or ((Startbahnhof = 13) and (Zielbahnhof = 12)) or
+     ((Startbahnhof = 13) and (Zielbahnhof = 14)) or ((Startbahnhof = 14) and (Zielbahnhof = 13)) or
+     ((Startbahnhof = 14) and (Zielbahnhof = 16)) or ((Startbahnhof = 16) and (Zielbahnhof = 14)) or
+     ((Startbahnhof = 15) and (Zielbahnhof = 16)) or ((Startbahnhof = 16) and (Zielbahnhof = 15)) or
+     ((Startbahnhof = 15) and (Zielbahnhof = 17)) or ((Startbahnhof = 17) and (Zielbahnhof = 15)) or
+     ((Startbahnhof = 16) and (Zielbahnhof = 17)) or ((Startbahnhof = 17) and (Zielbahnhof = 16)) then
   begin
-    TemporaerZwischenstation := Zwischenstation;
-    Zwischenstation := Zielbahnhof;
-    Zielbahnhof := TemporaerZwischenstation;
+    Kosten := ZWISCHENSTATIONPREIS;
+    DirektGefunden := True;
+  end
+  else if ((Startbahnhof = 3) and (Zielbahnhof = 5)) or ((Startbahnhof = 5) and (Zielbahnhof = 3)) or
+          ((Startbahnhof = 4) and (Zielbahnhof = 8)) or ((Startbahnhof = 8) and (Zielbahnhof = 4)) or
+          ((Startbahnhof = 6) and (Zielbahnhof = 8)) or ((Startbahnhof = 8) and (Zielbahnhof = 6)) or
+          ((Startbahnhof = 9) and (Zielbahnhof = 15)) or ((Startbahnhof = 15) and (Zielbahnhof = 9)) or
+          ((Startbahnhof = 14) and (Zielbahnhof = 15)) or ((Startbahnhof = 15) and (Zielbahnhof = 14)) then
+  begin
+    Kosten := SCHNELLVERBINDUNGPREIS;
+    DirektGefunden := True;
   end;
 
-  if ((Zwischenstation = 1) and ((Zielbahnhof = 2) or (Zielbahnhof = 3))) or
-     ((Zwischenstation = 2) and (Zielbahnhof = 5)) or
-     ((Zwischenstation = 3) and (Zielbahnhof = 4)) or
-     ((Zwischenstation = 4) and (Zielbahnhof = 6)) or
-     ((Zwischenstation = 5) and ((Zielbahnhof = 7) or (Zielbahnhof = 10))) or
-     ((Zwischenstation = 6) and ((Zielbahnhof = 7) or (Zielbahnhof = 9))) or
-     ((Zwischenstation = 7) and (Zielbahnhof = 10)) or
-     ((Zwischenstation = 8) and ((Zielbahnhof = 9) or (Zielbahnhof = 12))) or
-     ((Zwischenstation = 9) and ((Zielbahnhof = 10) or (Zielbahnhof = 14))) or
-     ((Zwischenstation = 10) and (Zielbahnhof = 11)) or
-     ((Zwischenstation = 12) and (Zielbahnhof = 13)) or
-     ((Zwischenstation = 12) and (Zielbahnhof = 14)) or
-     ((Zwischenstation = 13) and (Zielbahnhof = 14)) or
-     ((Zwischenstation = 14) and (Zielbahnhof = 16)) or
-     ((Zwischenstation = 15) and ((Zielbahnhof = 16) or (Zielbahnhof = 17))) or
-     ((Zwischenstation = 16) and (Zielbahnhof = 17))
-  then
+  if DirektGefunden then
   begin
-    BenachbartZwischenstation := true;
-    SchnellverbindungZwischenstation := False;
-  end
-  else if ((Zwischenstation = 4) and (Zielbahnhof = 8)) or
-          ((Zwischenstation = 3) and (Zielbahnhof = 5)) or
-          ((Zwischenstation = 14) and (Zielbahnhof = 15)) or
-          ((Zwischenstation = 10) and (Zielbahnhof = 15)) then
-  begin
-    SchnellverbindungZwischenstation := True;
-    BenachbartZwischenstation := false;
-  end
-  else
-  begin
-    BenachbartZwischenstation := false;
-    SchnellverbindungZwischenstation := False;
+    Writeln('Gesamtkosten: ', Kosten, ' Euro');
+    Halt;
   end;
 
-  if ((Startbahnhof = 1) and (Zielbahnhof = 2)) or
-     ((Startbahnhof = 2) and (Zielbahnhof = 5) or (Zielbahnhof = 3)) or
-     ((Startbahnhof = 3) and (Zielbahnhof = 4)) or
-     ((Startbahnhof = 4) and (Zielbahnhof = 6)) or
-     ((Startbahnhof = 5) and ((Zielbahnhof = 7) or (Zielbahnhof = 10))) or
-     ((Startbahnhof = 6) and ((Zielbahnhof = 7) or (Zielbahnhof = 9))) or
-     ((Startbahnhof = 7) and (Zielbahnhof = 10)) or
-     ((Startbahnhof = 8) and ((Zielbahnhof = 9) or (Zielbahnhof = 12))) or
-     ((Startbahnhof = 9) and ((Zielbahnhof = 10) or (Zielbahnhof = 14))) or
-     ((Startbahnhof = 10) and (Zielbahnhof = 11)) or
-     ((Startbahnhof = 12) and (Zielbahnhof = 13)) or
-     ((Startbahnhof = 12) and (Zielbahnhof = 14)) or
-     ((Startbahnhof = 13) and (Zielbahnhof = 14)) or
-     ((Startbahnhof = 14) and (Zielbahnhof = 16)) or
-     ((Startbahnhof = 15) and ((Zielbahnhof = 16) or (Zielbahnhof = 17))) or
-     ((Startbahnhof = 16) and (Zielbahnhof = 17))
-  then
+  Writeln('Keine direkte Verbindung. Bitte eine Zwischenstation eingeben (1-17):');
+  Readln(Zwischenstation);
+
+  if (Zwischenstation = Startbahnhof) or (Zwischenstation = Zielbahnhof) then
   begin
-    Benachbart := true;
-    Schnellverbindung := False;
+    Writeln('Zwischenstation darf nicht Start oder Ziel sein.');
+    Halt;
+  end;
+
+  DirektGefunden := False;
+
+  { Start -> Zwischenstation prüfen }
+  if ((Startbahnhof = 1) and (Zwischenstation = 3)) or ((Startbahnhof = 3) and (Zwischenstation = 1)) or
+     ((Startbahnhof = 2) and (Zwischenstation = 3)) or ((Startbahnhof = 3) and (Zwischenstation = 2)) or
+     ((Startbahnhof = 2) and (Zwischenstation = 5)) or ((Startbahnhof = 5) and (Zwischenstation = 2)) or
+     ((Startbahnhof = 3) and (Zwischenstation = 4)) or ((Startbahnhof = 4) and (Zwischenstation = 3)) or
+     ((Startbahnhof = 3) and (Zwischenstation = 6)) or ((Startbahnhof = 6) and (Zwischenstation = 3)) or
+     ((Startbahnhof = 4) and (Zwischenstation = 6)) or ((Startbahnhof = 6) and (Zwischenstation = 4)) or
+     ((Startbahnhof = 5) and (Zwischenstation = 7)) or ((Startbahnhof = 7) and (Zwischenstation = 5)) or
+     ((Startbahnhof = 6) and (Zwischenstation = 7)) or ((Startbahnhof = 7) and (Zwischenstation = 6)) or
+     ((Startbahnhof = 7) and (Zwischenstation = 10)) or ((Startbahnhof = 10) and (Zwischenstation = 7)) or
+     ((Startbahnhof = 8) and (Zwischenstation = 9)) or ((Startbahnhof = 9) and (Zwischenstation = 8)) or
+     ((Startbahnhof = 8) and (Zwischenstation = 12)) or ((Startbahnhof = 12) and (Zwischenstation = 8)) or
+     ((Startbahnhof = 9) and (Zwischenstation = 15)) or ((Startbahnhof = 15) and (Zwischenstation = 9)) or
+     ((Startbahnhof = 10) and (Zwischenstation = 11)) or ((Startbahnhof = 11) and (Zwischenstation = 10)) or
+     ((Startbahnhof = 12) and (Zwischenstation = 13)) or ((Startbahnhof = 13) and (Zwischenstation = 12)) or
+     ((Startbahnhof = 13) and (Zwischenstation = 14)) or ((Startbahnhof = 14) and (Zwischenstation = 13)) or
+     ((Startbahnhof = 14) and (Zwischenstation = 16)) or ((Startbahnhof = 16) and (Zwischenstation = 14)) or
+     ((Startbahnhof = 15) and (Zwischenstation = 16)) or ((Startbahnhof = 16) and (Zwischenstation = 15)) or
+     ((Startbahnhof = 15) and (Zwischenstation = 17)) or ((Startbahnhof = 17) and (Zwischenstation = 15)) or
+     ((Startbahnhof = 16) and (Zwischenstation = 17)) or ((Startbahnhof = 17) and (Zwischenstation = 16)) then
+  begin
+    Kosten := ZWISCHENSTATIONPREIS;
+    DirektGefunden := True;
   end
-  else if ((Startbahnhof = 4) and (Zielbahnhof = 8)) or
-          ((Startbahnhof = 3) and (Zielbahnhof = 5)) or
-          ((Startbahnhof = 14) and (Zielbahnhof = 15)) or
-          ((Startbahnhof = 10) and (Zielbahnhof = 15)) then
+  else if ((Startbahnhof = 3) and (Zwischenstation = 5)) or ((Startbahnhof = 5) and (Zwischenstation = 3)) or
+          ((Startbahnhof = 4) and (Zwischenstation = 8)) or ((Startbahnhof = 8) and (Zwischenstation = 4)) or
+          ((Startbahnhof = 6) and (Zwischenstation = 8)) or ((Startbahnhof = 8) and (Zwischenstation = 6)) or
+          ((Startbahnhof = 9) and (Zwischenstation = 15)) or ((Startbahnhof = 15) and (Zwischenstation = 9)) or
+          ((Startbahnhof = 14) and (Zwischenstation = 15)) or ((Startbahnhof = 15) and (Zwischenstation = 14)) then
   begin
-    Schnellverbindung := True;
-    Benachbart := false;
+    Kosten := SCHNELLVERBINDUNGPREIS;
+    DirektGefunden := True;
+  end;
+
+  if not DirektGefunden then
+  begin
+    Writeln('Keine Verbindung vom Start zur Zwischenstation.');
+    Halt;
+  end;
+
+  DirektGefunden := False;
+
+  { Zwischenstation -> Zielbahnhof prüfen }
+  if ((Zwischenstation = 1) and (Zielbahnhof = 3)) or ((Zwischenstation = 3) and (Zielbahnhof = 1)) or
+     ((Zwischenstation = 2) and (Zielbahnhof = 3)) or ((Zwischenstation = 3) and (Zielbahnhof = 2)) or
+     ((Zwischenstation = 2) and (Zielbahnhof = 5)) or ((Zwischenstation = 5) and (Zielbahnhof = 2)) or
+     ((Zwischenstation = 3) and (Zielbahnhof = 4)) or ((Zwischenstation = 4) and (Zielbahnhof = 3)) or
+     ((Zwischenstation = 3) and (Zielbahnhof = 6)) or ((Zwischenstation = 6) and (Zielbahnhof = 3)) or
+     ((Zwischenstation = 4) and (Zielbahnhof = 6)) or ((Zwischenstation = 6) and (Zielbahnhof = 4)) or
+     ((Zwischenstation = 5) and (Zielbahnhof = 7)) or ((Zwischenstation = 7) and (Zielbahnhof = 5)) or
+     ((Zwischenstation = 6) and (Zielbahnhof = 7)) or ((Zwischenstation = 7) and (Zielbahnhof = 6)) or
+     ((Zwischenstation = 7) and (Zielbahnhof = 10)) or ((Zwischenstation = 10) and (Zielbahnhof = 7)) or
+     ((Zwischenstation = 8) and (Zielbahnhof = 9)) or ((Zwischenstation = 9) and (Zielbahnhof = 8)) or
+     ((Zwischenstation = 8) and (Zielbahnhof = 12)) or ((Zwischenstation = 12) and (Zielbahnhof = 8)) or
+     ((Zwischenstation = 9) and (Zielbahnhof = 15)) or ((Zwischenstation = 15) and (Zielbahnhof = 9)) or
+     ((Zwischenstation = 10) and (Zielbahnhof = 11)) or ((Zwischenstation = 11) and (Zielbahnhof = 10)) or
+     ((Zwischenstation = 12) and (Zielbahnhof = 13)) or ((Zwischenstation = 13) and (Zielbahnhof = 12)) or
+     ((Zwischenstation = 13) and (Zielbahnhof = 14)) or ((Zwischenstation = 14) and (Zielbahnhof = 13)) or
+     ((Zwischenstation = 14) and (Zielbahnhof = 16)) or ((Zwischenstation = 16) and (Zielbahnhof = 14)) or
+     ((Zwischenstation = 15) and (Zielbahnhof = 16)) or ((Zwischenstation = 16) and (Zielbahnhof = 15)) or
+     ((Zwischenstation = 15) and (Zielbahnhof = 17)) or ((Zwischenstation = 17) and (Zielbahnhof = 15)) or
+     ((Zwischenstation = 16) and (Zielbahnhof = 17)) or ((Zwischenstation = 17) and (Zielbahnhof = 16)) then
+  begin
+    Kosten := Kosten + ZWISCHENSTATIONPREIS;
+    DirektGefunden := True;
+  end
+  else if ((Zwischenstation = 3) and (Zielbahnhof = 5)) or ((Zwischenstation = 5) and (Zielbahnhof = 3)) or
+          ((Zwischenstation = 4) and (Zielbahnhof = 8)) or ((Zwischenstation = 8) and (Zielbahnhof = 4)) or
+          ((Zwischenstation = 6) and (Zielbahnhof = 8)) or ((Zwischenstation = 8) and (Zielbahnhof = 6)) or
+          ((Zwischenstation = 9) and (Zielbahnhof = 15)) or ((Zwischenstation = 15) and (Zielbahnhof = 9)) or
+          ((Zwischenstation = 14) and (Zielbahnhof = 15)) or ((Zwischenstation = 15) and (Zielbahnhof = 14)) then
+  begin
     Kosten := Kosten + SCHNELLVERBINDUNGPREIS;
-  end
-  else
-  begin
-    Benachbart := false;
-    Schnellverbindung := False;
+    DirektGefunden := True;
   end;
 
-  if ((Benachbart = false) and (Schnellverbindung = false)) then
+  if not DirektGefunden then
   begin
-    Writeln('Geben sie eine Zwischenstation aus von der sie weiter Reisen möchten');
-    Readln(Zwischenstationen);
+    Writeln('Keine Verbindung von Zwischenstation zum Zielbahnhof.');
+    Halt;
   end;
 
-  if Temporaer > 0 then
-  begin
-    Temporaer := Zielbahnhof;
-    Zielbahnhof := Startbahnhof;
-    Startbahnhof := Temporaer;
-  end;
-
-  if TemporaerZwischenstation > 0 then
-  begin
-    TemporaerZwischenstation := Zielbahnhof;
-    Zielbahnhof := Zwischenstationen;
-    Zwischenstation := TemporaerZwischenstation;
-  end;
-
-  case Startbahnhof of
-    1: Writeln('Der Startbahnhof ist Kiel');
-    2: Writeln('Der Startbahnhof ist Rostock');
-    3: Writeln('Der Startbahnhof ist Hamburg');
-    4: Writeln('Der Startbahnhof ist Bremen');
-    5: Writeln('Der Startbahnhof ist Berlin');
-    6: Writeln('Der Startbahnhof ist Hannover');
-    7: Writeln('Der Startbahnhof ist Magdeburg');
-    8: Writeln('Der Startbahnhof ist Dortmund');
-    9: Writeln('Der Startbahnhof ist Kassel');
-    10: Writeln('Der Startbahnhof ist Leipzig');
-    11: Writeln('Der Startbahnhof ist Dresden');
-    12: Writeln('Der Startbahnhof ist Koeln');
-    13: Writeln('Der Startbahnhof ist Bonn');
-    14: Writeln('Der Startbahnhof ist Frankfurt');
-    15: Writeln('Der Startbahnhof ist Nuernberg');
-    16: Writeln('Der Startbahnhof ist Stuttgart');
-    17: Writeln('Der Startbahnhof ist Muenchen');
-  else
-    Writeln('Fehlerhafte Eingabe!');
-  end;
-
-  case Zielbahnhof of
-    1: Writeln('Der Zielbahnhof ist Kiel');
-    2: Writeln('Der Zielbahnhof ist Rostock');
-    3: Writeln('Der Zielbahnhof ist Hamburg');
-    4: Writeln('Der Zielbahnhof ist Bremen');
-    5: Writeln('Der Zielbahnhof ist Berlin');
-    6: Writeln('Der Zielbahnhof ist Hannover');
-    7: Writeln('Der Zielbahnhof ist Magdeburg');
-    8: Writeln('Der Zielbahnhof ist Dortmund');
-    9: Writeln('Der Zielbahnhof ist Kassel');
-    10: Writeln('Der Zielbahnhof ist Leipzig');
-    11: Writeln('Der Zielbahnhof ist Dresden');
-    12: Writeln('Der Zielbahnhof ist Koeln');
-    13: Writeln('Der Zielbahnhof ist Bonn');
-    14: Writeln('Der Zielbahnhof ist Frankfurt');
-    15: Writeln('Der Zielbahnhof ist Nuernberg');
-    16: Writeln('Der Zielbahnhof ist Stuttgart');
-    17: Writeln('Der Zielbahnhof ist Muenchen');
-  else
-    Writeln('Fehlerhafte Eingabe!');
-  end;
-
-  case Zwischenstationen of
-    1: Writeln('Der Zwischenstopp liegt in Kiel');
-    2: Writeln('Der Zwischenstopp liegt in Rostock');
-    3: Writeln('Der Zwischenstopp liegt in Hamburg');
-    4: Writeln('Der Zwischenstopp liegt in Bremen');
-    5: Writeln('Der Zwischenstopp liegt in Berlin');
-    6: Writeln('Der Zwischenstopp liegt in Hannover');
-    7: Writeln('Der Zwischenstopp liegt in Magdeburg');
-    8: Writeln('Der Zwischenstopp liegt in Dortmund');
-    9: Writeln('Der Zwischenstopp liegt in Kassel');
-    10: Writeln('Der Zwischenstopp liegt in Leipzig');
-    11: Writeln('Der Zwischenstopp liegt in Dresden');
-    12: Writeln('Der Zwischenstopp liegt in Koeln');
-    13: Writeln('Der Zwischenstopp liegt in Bonn');
-    14: Writeln('Der Zwischenstopp liegt in Frankfurt');
-    15: Writeln('Der Zwischenstopp liegt in Nuernberg');
-    16: Writeln('Der Zwischenstopp liegt in Stuttgart');
-    17: Writeln('Der Zwischenstopp liegt in Muenchen');
-  else
-    Writeln('Fehlerhafte Eingabe!');
-  end;
-
-  if Schnellverbindung = true then
-begin
-  Kosten := Kosten + SCHNELLVERBINDUNGPREIS;
-end
-else if Benachbart = true then
-begin
-  Kosten := Kosten + ZWISCHENSTATIONPREIS;
-end
-else if BenachbartZwischenstation = true then
-begin
-  Kosten := KostenZwischenstation + ZWISCHENSTATIONPREIS;
-end
-else if SchnellverbindungZwischenstation = true then
-begin
-  Kosten := KostenZwischenstation + SCHNELLVERBINDUNGPREIS;
-end;
+  Writeln('Gesamtkosten: ', Kosten, ' Euro');
+end.
 
 
- KostenInsgesamt := Kosten + KostenZwischenstation;
-
-  if Zwischenstationen = Startbahnhof then
-  begin
-    Writeln('Sie sind bereits am Zielbahnhof angekommen!');
-  end
-  else if Zwischenstationen = Zielbahnhof then
-  begin
-    Writeln('Die Zwischenstation ist gleich dem Zielbahnhof!');
-  end
-  else if Startbahnhof = Zielbahnhof then
-  begin
-    Writeln('Der Startbahnhof ist gleich dem Zielbahnhof!');
-  end
-  else
-  begin
-
-    Writeln('Die Kosten betragen: ', KostenInsgesamt, ' Euro');
-  end;
 end.
