acd8ab15e3b297da789facd586eb4567b6e4fb88
Arabcheck <akiel.leaibi13@gmail.com>
2025-04-28 12:26:25 +0200

    Update aufgabe3WIP.pas

--- aufgabe3WIP.pas
+++ aufgabe3WIP.pas
@@ -6,23 +6,27 @@ uses
 var
   Startbahnhof: Byte;
   Zielbahnhof: Byte;
-  Zwischenstationen: Byte;
+  Zwischenstation: Byte;
   Kosten: Integer;
   Benachbart: Boolean;
   Schnellverbindung: Boolean;
   Temporaer: Byte;
+  Benachbartzwischnestation: Boolean;
+  Schnellverbindungzwischnestation: Boolean;
 
 const
-  ZWISCHENSTATIONPREIS = 100;
-  SCHNELLVERBINDUNGPREIS = 70;
+  ZWISCHENSTATIONPREIS = 70;
+  SCHNELLVERBINDUNGPREIS = 100;
 
 begin
   Startbahnhof := 0;
   Zielbahnhof := 0;
-  Zwischenstationen := 0;
+  Zwischenstation := 0;
   Kosten := 0;
   Benachbart := False;
   Schnellverbindung := False;
+  Benachbartzwischnestation := False;
+  Schnellverbindungzwischnestation := False;
 
   Writeln('Geben sie an von welchem Bahnhof sie starten');
   Readln(Startbahnhof);
@@ -34,6 +38,48 @@ begin
     Temporaer := Startbahnhof;
     Startbahnhof := Zielbahnhof;
     Zielbahnhof := Temporaer;
+  end;
+
+    if Zwischenstation > Zielbahnhof then
+  begin
+    Temporaerzwischentation := Startbahnhof;
+    Zwischenstation := Zielbahnhof;
+    Zielbahnhof := Temporaerzwischentation;
+  end;
+
+
+    if ((Zwischenstation = 1) and ((Zielbahnhof = 2) or (Zielbahnhof = 3))) or
+     ((Zwischenstation = 2) and (Zielbahnhof = 5)) or
+     ((Zwischenstation = 3) and (Zielbahnhof = 4)) or
+     ((Zwischenstation = 4) and (Zielbahnhof = 6)) or
+     ((Zwischenstation = 5) and ((Zielbahnhof = 7) or (Zielbahnhof = 10))) or
+     ((Zwischenstation = 6) and ((Zielbahnhof = 7) or (Zielbahnhof = 9))) or
+     ((Zwischenstation = 7) and (Zielbahnhof = 10)) or
+     ((Zwischenstation = 8) and ((Zielbahnhof = 9) or (Zielbahnhof = 12))) or
+     ((Zwischenstation = 9) and ((Zielbahnhof = 10) or (Zielbahnhof = 14))) or
+     ((Zwischenstation = 10) and (Zielbahnhof = 11)) or
+     ((Zwischenstation = 12) and (Zielbahnhof = 13)) or
+     ((Zwischenstation = 12) and (Zielbahnhof = 14)) or
+     ((Zwischenstation = 13) and (Zielbahnhof = 14)) or
+     ((Zwischenstation = 14) and (Zielbahnhof = 16)) or
+     ((Zwischenstation = 15) and ((Zielbahnhof = 16) or (Zielbahnhof = 17))) or
+     ((Zwischenstation = 16) and (Zielbahnhof = 17))
+  then
+  begin
+    BenachbartZwischenstation := true;
+  end;
+  else if ((Zwischenstation = 4) and (Zielbahnhof = 8)) or
+          ((Zwischenstation = 3) and (Zielbahnhof = 5)) or
+          ((Zwischenstation = 14) and (Zielbahnhof = 15)) or
+          ((Zwischenstation = 10) and (Zielbahnhof = 15)) then
+  begin
+    SchnellverbindungZwischenstation := True;
+    BenachbartZwischenstation := True;
+  end
+  else
+  begin
+    BenachbartZwischenstation := false;
+    SchnellverbindungZwischenstation := False;
   end;
 
   if ((Startbahnhof = 1) and ((Zielbahnhof = 2) or (Zielbahnhof = 3))) or
@@ -56,7 +102,7 @@ begin
   begin
     Benachbart := true;
     Schnellverbindung := False;
-    Kosten := Kosten + ZWISCHENSTATIONPREIS;
+    Kosten := Kosten + ZWISCHENSTATIONPREIS * 2;
   end
   else if ((Startbahnhof = 4) and (Zielbahnhof = 8)) or
           ((Startbahnhof = 3) and (Zielbahnhof = 5)) or
@@ -74,7 +120,7 @@ begin
   end;
 
 
-    if Benachbart = false then
+    if Benachbart = false and Schnellverbindung = false then
     begin
           Writeln('Geben sie eine Zwischenstation aus von der sie weiter Reisen möchten');
         Readln(Zwischenstationen);
@@ -85,6 +131,14 @@ begin
   Temporaer := Zielbahnhof;
   Zielbahnhof := Startbahnhof;
   Startbahnhof := Temporaer;
+   end;
+
+
+    if Temporaerzwischentation > 0 then
+ begin
+  Temporaerzwischentation := Zielbahnhof;
+  Zielbahnhof := Zwischenstationen;
+  Zwischenstation := Temporaerzwischentation;
    end;
 
 
@@ -174,7 +228,6 @@ begin
   end
   else
   begin
-    Kosten := Kosten + Startbahnhof + Zwischenstationen + Zielbahnhof;
     Writeln('Die Kosten betragen: ', Kosten, ' Euro');
   end;
 end.
